f791f44c1d66a482ff5ce94b560d15dc3ac205c1
Nikolay Golovin <iimonolitii@mail.ru>
2024-08-04 19:28:03 +0300

    init

---
+++ api/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "ru.netology"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(project(":db"))
+    testImplementation(project(":service"))
+    implementation(project(":service"))
+    implementation(project(":db"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
